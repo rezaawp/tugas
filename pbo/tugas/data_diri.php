88708eca3be656c018d1af57804b7a3e357e64bf
rezaawp <rezaaawp@gmail.com>
2022-10-03 12:17:01 +0700

    tugas pertama

---
+++ pbo/tugas/data_diri.php
@@ -0,0 +1,9 @@
+<form action="proses.php" method="get">
+	<p>Nama : <input type="text" name="nama" id=""></p>
+	<p>Umur : <input type="number" name="umur"></p>
+	<p>Alamat : <input type="password" name="umur"></p>
+	<p>Jenis Kelamin : <input type="radio" name="jk"> Laki laki <input type="radio" name="jk" value="Perempuan"> Perempuan</p>
+	<p>Tanggal Lahir : <input type="date" name="tl"></p>
+
+	<button type="submit">Daftar</button>
+</form>
---
+++ pbo/tugas/index.php
@@ -0,0 +1,7 @@
+<form action="proses.php" method="get">
+	<p>Nama : <input type="text" name="name" id=""></p>
+	<p>Password : <input type="password" name="password"></p>
+	<p>Umur : <input type="password" name="umur"></p>
+
+	<button type="submit">Simpan</button>
+</form>
