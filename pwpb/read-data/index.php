7c6741445c5e911df7f132ea64ba5434873ed7ec
rezaawp <rezaaawp@gmai.com>
2023-01-19 12:03:36 +0700

    update tugas pwpb

---
+++ pwpb/read-data/index.php
@@ -0,0 +1,29 @@
+<?php
+$koneksi = new PDO('mysql:host=localhost;dbname=sekolah', 'root', '');
+$query = $koneksi->query('SELECT * FROM siswa');
+
+$data = $query->fetchAll();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Read Data</title>
+</head>
+
+<body>
+    <h1>List Data Siswa</h1>
+
+    <?php for($i = 0; $i < count($data); $i++) : ?>
+        <?php echo $i == 0 ? "<hr>" : "" ?>
+        <p>Username : <?= $data[$i]['nama'] ?></p>
+        <p>Nama : <?= $data[$i]['username'] ?></p>
+        <p>Password : <?= $data[$i]['password'] ?></p>
+        <hr>
+    <?php endfor; ?>
+</body>
+
+</html>
