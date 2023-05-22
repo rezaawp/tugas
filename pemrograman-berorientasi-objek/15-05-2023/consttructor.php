4c84cd3c21e03d87eff980862abcf9565d5b6e2e
rezawp <rezaaawp@gmail.com>
2023-05-22 11:40:36 +0700

    update: tugas 22 may 2023

--- pemrograman-berorientasi-objek/15-05-2023/consttructor.php
+++ pemrograman-berorientasi-objek/15-05-2023/consttructor.php
@@ -4,12 +4,41 @@ class Mahasiswa
     public $nim = "001";
     public $nama = "Reza Khoirul Wijaya Putra";
     public $jurusan = "PPLG";
+    private $errNim = "";
+    private $errNama = "";
+    private $errJurusan = "";
 
     function __construct($nim, $nama, $jurusan)
     {
         $this->nim = $nim;
         $this->nama = $nama;
         $this->jurusan = $jurusan;
+    }
+
+    function validasi()
+    {
+        $status = true;
+        if ($this->nim == "") {
+            $this->errNim = "Nim harus di isi";
+            $status = false;
+        }
+        if ($this->nama == "") {
+            $this->errNama = "Nama harus di isi";
+            $status = false;
+        }
+        if ($this->jurusan == "") {
+            $this->errJurusan = "Jurusan harus di isi";
+            $status = false;
+        }
+
+        return $status;
+    }
+
+    function errorr_messages()
+    {
+        echo $this->errNim . '<br>';
+        echo $this->errNama . '<br>';
+        echo $this->errJurusan . '<br>';
     }
 
     function tampil_biodata()
@@ -21,4 +50,8 @@ class Mahasiswa
 }
 $mahasiswa = new Mahasiswa($_POST['nim'], $_POST['nama'], $_POST['jurusan']);
 
-$mahasiswa->tampil_biodata();
+if ($mahasiswa->validasi()) {
+    $mahasiswa->tampil_biodata();
+} else {
+    $mahasiswa->errorr_messages();
+}
